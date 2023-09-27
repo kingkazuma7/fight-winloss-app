970f3aca0f9adbaf816d7df6ab46f8cca9f4b104
kingkazuma7 <your.nasionasio7@gmail.com>
2023-09-27 20:25:10 +0900

    FightList〜FighterSelectionまで

--- src/components/FightList.jsx
+++ src/components/FightList.jsx
@@ -1,8 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
+import FighterSelection from './FighterSelection'
+import FightSummary from './FightSummary'
 
 const FightList = ({ fights }) => {
+  const [selections, setSelections] = useState({});
+  
+  const handleSelection = (fightId, fighter) => (
+    setSelections({ ...selections, [fightId]: fighter })
+  )
+  
   return (
-    <div>FightList</div>
+    <div>
+      {fights.map((fight) => (
+        <FighterSelection
+          key={fight.id}
+          fight={fight}
+          onSelection={(fighter) => handleSelection(fight.id, fighter)}
+        />
+      ))}
+      <FightSummary />
+    </div>
   )
 }
 
